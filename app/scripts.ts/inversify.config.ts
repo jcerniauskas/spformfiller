f3757de4929c8a8500d384528c68f1c028a84d94
Justas Černiauskas <koperfildas@gmail.com>
2016-12-31 10:20:02 +0200

    Cleaned up dependencies, removed Bower

--- app/scripts.ts/inversify.config.ts
+++ app/scripts.ts/inversify.config.ts
@@ -1,4 +1,4 @@
-import "reflect-metadata";  //this is a required polyfill for inversify and should only be imported once in the application
+import "reflect-metadata";  // this is a required polyfill for inversify and should only be imported once in the application
 import { IAlertService, TYPES } from "./IAlertService";
 import { inject, injectable, Container } from "inversify";
 import AlertService from "./AlertService";
