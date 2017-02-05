ed09472529da4198e438b8a09dc69fab4cbe0268
Justas Černiauskas <koperfildas@gmail.com>
2017-02-05 22:35:21 +0200

    People picker clears the previously entered values

--- app/scripts.ts/Providers/FieldValueWriter/PeopleFieldValueWriter.ts
+++ app/scripts.ts/Providers/FieldValueWriter/PeopleFieldValueWriter.ts
@@ -4,7 +4,7 @@ import { injectable } from "inversify";
 import { FieldValueWriterBase } from "./FieldValueWriterBase";
 import { User } from "../../Services/IUserService";
 
-// this class writes a value to a text field (which should be a simple input field)
+// this class writes a value to a user field
 @injectable()
 export default class PeopleFieldValueWriter extends FieldValueWriterBase {
     public WriteValue(fieldInfo: IFieldInfo, value: User): void {
@@ -12,6 +12,16 @@ export default class PeopleFieldValueWriter extends FieldValueWriterBase {
         const editorControl = super.GetInputControlForField(fieldInfo, "input", "$ClientPeoplePicker_EditorInput");
 
         const peoplePickerSPControl = SPClientPeoplePicker.SPClientPeoplePickerDict[peoplePickerDiv[0].id];
+
+        // clear users
+        if (peoplePickerSPControl) {
+            const resolvedUsers = $(document.getElementById(peoplePickerSPControl.ResolvedListElementId)).find("span[class='sp-peoplepicker-userSpan']");
+            $(resolvedUsers).each(function (index) {
+                peoplePickerSPControl.DeleteProcessedUser(this);
+            });
+        }
+
+        // fill the value
         editorControl.val(value.LoginName);
         peoplePickerSPControl.AddUnresolvedUserFromEditor(true);
     }
