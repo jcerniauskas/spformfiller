5443d2d3c1abdcf8b02a35898020ffe8ca61c56e
Justas Černiauskas <koperfildas@gmail.com>
2017-03-01 00:09:06 +0200

    Fixes and refactoring to date format

--- app/scripts.ts/Providers/FieldValueWriter/DateFieldValueWriter.ts
+++ app/scripts.ts/Providers/FieldValueWriter/DateFieldValueWriter.ts
@@ -2,22 +2,19 @@ import { IFieldValueWriter } from "./IFieldValueWriter";
 import { IFieldInfo, IDateFieldInfo, DateFormat } from "../../FieldInfo/IFieldInfo";
 import { injectable, inject } from "inversify";
 import { FieldValueWriterBase } from "./FieldValueWriterBase";
-import { IDateFormatService } from "../../Services/DateFormat/IDateFormatService";
+import { IDateValue } from "../ValueTypes/IDateValue";
 
 // this class writes a value to a date field
 @injectable()
 export class DateFieldValueWriter extends FieldValueWriterBase {
-    constructor(@inject("IDateFormatService") private _dateFormatService: IDateFormatService) {
-        super();
-    }
+    
 
-    public async WriteValue(fieldInfo: IDateFieldInfo, value: any): Promise<void> {
+    public WriteValue(fieldInfo: IDateFieldInfo, value: any): void {
         const dateInputField = super.GetInputControlForField(fieldInfo, "input", "$DateTimeFieldDate");
 
-        const dateValue = <Date>value;
-        // TODO: test for regional settings effect
-        //const formattedValue = `${dateValue.getMonth() + 1}/${dateValue.getDate()}/${dateValue.getFullYear()}`;
-        const formattedValue = await this._dateFormatService.GetFormattedDate(dateValue);
+        const dateValue = <IDateValue>value;
+        
+        const formattedValue = dateValue.FormattedDate;
         dateInputField.val(formattedValue);
 
         // check if this is a Date + Time field
@@ -25,10 +22,10 @@ export class DateFieldValueWriter extends FieldValueWriterBase {
             // in this case we must also update the hour and minute values
             const hourSelectField = super.GetInputControlForField(fieldInfo, "select", "$DateTimeFieldDateHours");
             // TODO: test for regional settings effect
-            hourSelectField.val(dateValue.getHours());
+            hourSelectField.val(dateValue.Date.getHours());
 
             const minutesSelectField = super.GetInputControlForField(fieldInfo, "select", "$DateTimeFieldDateMinutes");
-            minutesSelectField.val(DateFieldValueWriter.PadZero(dateValue.getMinutes()));
+            minutesSelectField.val(DateFieldValueWriter.PadZero(dateValue.Date.getMinutes()));
         }
     }
 
